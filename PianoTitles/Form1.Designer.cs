ea42540f0f8bdce35d8666e9a9b4a1b32e2c04c9
Geo <Geo@DESKTOP-4HDFJN5>
2020-08-20 12:47:39 +0300

    Add project files.

---
+++ PianoTitles/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace PianoTitles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 525);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(102, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 523);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 20);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Incepe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(162, 494);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Gresite:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 493);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Corecte:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(215, 486);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(87, 486);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "0";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Hand;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 5;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.Ivory;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.RowTemplate.Height = 80;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(283, 467);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 603);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PianoTitles/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace PianoTitles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\scor.mdf;Integrated Security=True;Connect Timeout=30";
+        DataTable dt = new DataTable(); int rr = 5; int p = 0; int alt = 50; int alt2 = 10;
+        Image wrong = Properties.Resources.wrong;
+        int corect, gresit = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\Geo\source\repos\PianoTitles\PianoTitles\Teardrop1.wav");
+            player.Play();
+
+            dt.Columns.Add(new DataColumn("1", typeof(string)));
+            dt.Columns.Add(new DataColumn("2", typeof(string)));
+            dt.Columns.Add(new DataColumn("3", typeof(string)));
+            dt.Columns.Add(new DataColumn("4", typeof(string)));
+
+            dataGridView1.DataSource = dt;
+            dataGridView1.ClearSelection();
+
+            dt.Rows.Add("", "", "", ""); dt.Rows.Add("", "", "", ""); dt.Rows.Add("", "", "", ""); dt.Rows.Add("", "", "", ""); dt.Rows.Add("", "", "", "");
+            foreach (DataGridViewColumn c in dataGridView1.Columns)
+            { c.Width = 71; }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dt.Rows.Add("", "", "", "");
+            int rand = Convert.ToInt32(dt.Rows.Count - 1);
+            formatcell(rand);
+            foreach (DataGridViewColumn c in dataGridView1.Columns)
+            { c.Width = 71; }
+            if (rr >= 5)
+            {
+                verificare(0);
+                dataGridView1.Rows.RemoveAt(0);
+
+
+            }
+            rr++;
+
+            if (corect == 10 + p)
+            {
+                if (timer1.Interval > 100)
+                {
+                    p = p + 10;
+                    timer1.Interval = timer1.Interval - alt;
+                }
+                else
+                { p = p + 10; timer1.Interval = timer1.Interval - alt2; }
+            }
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            if (label2.Text == "10")
+            {
+                timer1.Stop();
+                if (textBox1.Text == "")
+                    textBox1.Text = "Anonymus";
+                SqlCommand cmd;
+                cmd = new SqlCommand("insert into scoruri(nume,scor) VALUES (@nm, @sc)", con);
+                cmd.Parameters.AddWithValue("nm", textBox1.Text);
+                cmd.Parameters.AddWithValue("sc", Convert.ToInt32(label1.Text));
+
+                cmd.ExecuteNonQuery();
+                DialogResult d = MessageBox.Show("Ai pierdut!", "Incepi un joc nou?", MessageBoxButtons.YesNo);
+                if (d == DialogResult.Yes)
+                {
+                    label1.Text = "0";
+                    label2.Text = "0";
+                    dt.Clear(); rr = 5; p = 0; alt = 50; alt2 = 10; gresit = 0; corect = 0;
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\Geo\source\repos\PianoTitles\PianoTitles\Teardrop1.wav");
+                    player.Play();
+
+                    dataGridView1.DataSource = dt;
+                    dataGridView1.ClearSelection();
+                    timer1.Start();
+
+                    dt.Rows.Add("", "", "", ""); dt.Rows.Add("", "", "", ""); dt.Rows.Add("", "", "", ""); dt.Rows.Add("", "", "", ""); dt.Rows.Add("", "", "", "");
+                    foreach (DataGridViewColumn c in dataGridView1.Columns)
+                    { c.Width = 71; }
+
+                }
+                else if (d == DialogResult.No)
+                {
+                    Application.Exit();
+                }
+            }
+        }
+        private void formatcell(int rand)
+        {
+            Random rnd = new Random();
+            int cell = rnd.Next(0, 4);
+            dataGridView1.Rows[rand].Cells[cell].Style.BackColor = Color.Black;
+        }
+        private void verificare(int rand)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (dataGridView1.Rows[rand].Cells[i].Style.BackColor == Color.Black)
+                {
+                    gresit++;
+                    label2.Text = gresit.ToString();
+                }
+            }
+        }
+        private void formatcellalb(int rand, int col)
+        {
+
+            dataGridView1.Rows[rand].Cells[col].Style.BackColor = Color.White;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor == Color.Black)
+            {
+                formatcellalb(e.RowIndex, e.ColumnIndex);
+                corect++;
+                label1.Text = corect.ToString();
+            }
+            else
+            {
+                formatcellwrong(e.RowIndex, e.ColumnIndex);
+                gresit++;
+                label2.Text = gresit.ToString();
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            button1.Enabled = false;
+        }
+
+        private void formatcellwrong(int rand, int col)
+        {
+
+            dataGridView1.Rows[rand].Cells[col].Style.BackColor = Color.Red;
+
+        }
+    }
+}
